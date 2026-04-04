15b29feca67ae42deece3b37e84fd0cb3854be46
ahir-arpit <ahir-arpit75055@example.com>
2026-04-05 01:18:14 +0530

    update work css

--- src/components/Career.tsx
+++ src/components/Career.tsx
@@ -5,8 +5,7 @@ const Career = () => {
     <div className="career-section section-container">
       <div className="career-container">
         <h2>
-          My career <span>&</span>
-          <br />
+          My <span>Career</span>
         </h2>
         <div className="career-info">
           <div className="career-timeline">
